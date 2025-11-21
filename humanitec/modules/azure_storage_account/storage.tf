dfd78a033645151c754765866e09960831715a13
Benjamin Chaudet <bchaudet@cegid.com>
2025-11-21 17:03:41 +0100

    fix

--- humanitec/modules/azure_storage_account/storage.tf
+++ humanitec/modules/azure_storage_account/storage.tf
@@ -1,14 +1,14 @@
-resource "azurerm_storage_account" "storage" {
-  name                            = lower(substr(replace(var.storage_account_name, "-", ""), 0, 15))
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  account_tier                    = "Standard"
-  account_replication_type        = "ZRS"
-  allow_nested_items_to_be_public = true
-}
+# resource "azurerm_storage_account" "storage" {
+#   name                            = lower(substr(replace(var.storage_account_name, "-", ""), 0, 15))
+#   resource_group_name             = var.resource_group_name
+#   location                        = var.location
+#   account_tier                    = "Standard"
+#   account_replication_type        = "ZRS"
+#   allow_nested_items_to_be_public = true
+# }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "default"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "blob"
-}
+# resource "azurerm_storage_container" "container" {
+#   name                  = "default"
+#   storage_account_name  = azurerm_storage_account.storage.name
+#   container_access_type = "blob"
+# }
