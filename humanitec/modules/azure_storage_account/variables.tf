f0e9451e4859b6c53b11e40851775d78c9efaff3
Benjamin Chaudet <bchaudet@cegid.com>
2025-11-21 16:05:18 +0100

    add storage module

---
+++ humanitec/modules/azure_storage_account/variables.tf
@@ -0,0 +1,20 @@
+variable "storage_account_name" {
+  description = "The name of the Azure Storage Account (will be made globally unique with suffix)."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location/region."
+  type        = string
+}
+
+variable "container_name" {
+  description = "The name of the Azure Storage Container."
+  type        = string
+  default     = "default"
+}
