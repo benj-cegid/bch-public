08db9388b237b1fb3c289464daf5b46dc64573dd
Benjamin Chaudet <bchaudet@cegid.com>
2025-11-24 14:22:20 +0100

    same resource as Humanitec

--- humanitec/modules/azure_storage_account/main.tf
+++ humanitec/modules/azure_storage_account/main.tf
@@ -6,17 +6,23 @@ terraform {
   }
 }
 
+resource "random_id" "suffix" {
+  byte_length = 8
+}
+
 resource "azurerm_storage_account" "storage" {
-  name                            = lower(substr(replace(var.storage_account_name, "-", ""), 0, 15))
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  account_tier                    = "Standard"
-  account_replication_type        = "ZRS"
+  name                     = "${substr(replace(var.storage_account_name, "-", ""), 0, 15)}${random_id.suffix.hex}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  # Allow blob public access for demo purposes (similar to GCS bucket)
   allow_nested_items_to_be_public = true
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_id    = azurerm_storage_account.storage.id
+  name                  = "default"
+  storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "blob"
 }
